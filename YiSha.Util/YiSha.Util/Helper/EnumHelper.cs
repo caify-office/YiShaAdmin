b233ac1c49ce217d03fa1b47621e3862704e2663
cai.fengyuan <cai.fengyuan@ruanyun.net>
2021-05-10 01:59:25 +0800

    添加EnumHelper

---
+++ YiSha.Util/YiSha.Util/Helper/EnumHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using YiSha.Util.Extension;
+
+namespace YiSha.Util.Helper
+{
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// 为有Flags特性的枚举类提供拆解后用逗号连接的值字符串
+        /// </summary>
+        public static string GetValueString<T>(T t) where T : System.Enum
+        {
+            var builder = new StringBuilder();
+            foreach (T value in System.Enum.GetValues(typeof(T)))
+            {
+                if ((t.ParseToInt() & value.ParseToInt()) != 0)
+                {
+                    builder.Append(value.ToString("d") + ",");
+                }
+            }
+            return builder.ToString().TrimEnd(',');
+        }
+
+        /// <summary>
+        /// 为有Flags特性的枚举类提供拆解后用逗号连接的Description字符串
+        /// </summary>
+        public static string GetDescription<T>(T t) where T : System.Enum
+        {
+            var builder = new StringBuilder();
+            foreach (T value in System.Enum.GetValues(typeof(T)))
+            {
+                if ((t.ParseToInt() & value.ParseToInt()) != 0)
+                {
+                    builder.Append(value.GetDescription() + ",");
+                }
+            }
+            return builder.ToString().TrimEnd(',');
+        }
+    }
+}
